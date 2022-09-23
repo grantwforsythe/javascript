ed3239f19e5e96f99c531a5fe5a9acdad807d0e0
Grant Forsythe <grantwforsythe@gmail.com>
2022-09-23 16:20:52 -0400

    Update misc/ajax

--- misc/ajax/app.js
+++ misc/ajax/app.js
@@ -1,4 +1,9 @@
-document.querySelector('#ajaxButton').addEventListener('click', () => {
+/**
+ * Make an AJAX request
+ * @param {string} fileName - The name of a local file on the server
+ * @returns {boolean} - The request was made
+ */
+const makeAJAXRequest = (fileName) => {
     let request = new XMLHttpRequest();
 
     if (!request) {
@@ -10,7 +15,7 @@ document.querySelector('#ajaxButton').addEventListener('click', () => {
         try {
             if (request.readyState === XMLHttpRequest.DONE) {
                 if (request.status === 200) {
-                    alert(request.responseText);
+                    parseFile(fileName, request);
                 } else {
                     alert('There was a problem making the request');
                 }
@@ -19,6 +24,27 @@ document.querySelector('#ajaxButton').addEventListener('click', () => {
             alert(`Caught Exception: ${e.description}`);
         }
     };
-    request.open('GET', 'test.txt');
+    request.open('GET', fileName);
     request.send();
-});
+};
+
+/**
+ * Parse a file's contents and alert the user.
+ * @param {string} fileName - The name of a local file on the server 
+ * @param {XMLHttpRequest} request - The AJAX request
+ */
+const parseFile = (fileName, request) => {
+    if (fileName.endsWith('txt')) {
+        alert(request.responseText);
+    } else if (fileName.endsWith('xml')) {
+        const xml = request.responseXML;
+        const rootNode = xml.querySelector('root');
+        alert(rootNode.firstChild.data);
+    } else {
+       throw new TypeError('File can only end in txt or xml') ;
+    }
+};
+
+document.querySelector('#button-text').addEventListener('click', makeAJAXRequest.bind(null, 'test.txt'));
+
+document.querySelector('#button-xml').addEventListener('click', makeAJAXRequest.bind(null, 'test.xml'));

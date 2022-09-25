852ac1d81e3f38a27e97f43b4f0b5900ad577b82
Grant Forsythe <grantwforsythe@gmail.com>
2022-09-25 15:49:45 -0400

    Update section7

--- section7/app.js
+++ section7/app.js
@@ -1,2 +1,19 @@
 const title = document.querySelector('#main-header');
+const ul = document.querySelector('#main-list');
+const section = document.querySelector('section');
+
 title.textContent = 'Title';
+section.style.backgroundColor = 'blue';
+
+// Toggle the visability of the list
+let visability = true;
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    section.style.display = visability ? 'none' : 'block';
+    visability = !visability;
+});
+
+// Creating elements
+const li = document.createElement('li');
+li.textContent = 'This was created in JavaScript';
+ul.appendChild(li);
